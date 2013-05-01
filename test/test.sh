5e62e80ef266cd81c04eea7d24f3e4bbb7312edb
Djoerd Hiemstra <hiemstra@cs.utwente.nl>
2013-05-01 20:03:06 +0000

    New test that includes BM25 results.

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 JAR=../mirex-0.3.jar
 
+#
+# Prepare
+#
 hadoop fs -mkdir warc-files
 hadoop fs -put test.warc.gz ./warc-files/
 hadoop fs -put wt2010-topics.queries-only ./
 hadoop jar $JAR nl.utwente.mirex.AnchorExtract warc-files/* anchors
+#
+# Key-value files
+#
 hadoop jar $JAR nl.utwente.mirex.TrecRun          KEYVAL anchors/* TrecOut wt2010-topics.queries-only
 hadoop fs -cat ./TrecOut/part*
 hadoop jar $JAR nl.utwente.mirex.QueryTermCount   KEYVAL anchors/*  wt2010-topics.queries-only  wt2010-topics.stats
@@ -20,3 +26,7 @@ hadoop jar $JAR nl.utwente.mirex.QueryTermCount   WARC warc-files/* wt2010-topic
 hadoop fs -cat ./wt2010-topics.stats2
 hadoop jar $JAR nl.utwente.mirex.TrecRunBaselines WARC warc-files/* BaselineOut2 wt2010-topics.stats2
 hadoop fs -cat ./BaselineOut2/part*
+
+# To remove:
+#hadoop fs -rmr BaselineOut* MIREX-tmp TrecOut* anchors warc-files wt2010-topics.queries-only wt2010-topics.stats*
+
